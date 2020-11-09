04a785003d5d155fd8221f8e4f42e4a854f42255
agwanyanjaba <bonkewycliffe@yahoo.com>
2020-11-09 23:12:33 +0300

    HashMap file

---
+++ src/com/wycliffe/codility/HashMapsTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wycliffe.codility;
+
+/**
+ *
+ * @author WOCHIENG1
+ */
+/*
+1. create hashmap as myHashMap
+2. Add items to myHashmap using a method addCities()
+3. Access the contemt of the HashMap
+4. Display the accessed elements of the hashmap
+*/
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.json.simple.JSONObject;
+public class HashMapsTest {
+    //Declare the Hashmap
+    static HashMap<Integer,String> citiesMap = new HashMap<>();
+    //define add cities method
+    public Object addCities(){
+       citiesMap.put(1, "Landon");
+       citiesMap.put(2, "Nairobi");
+       citiesMap.put(3, "Paris");
+       citiesMap.put(4, "Monrovia");
+       citiesMap.put(5, "Kigali");
+       
+        return citiesMap;
+    }
+    
+    public static void main(String args[]){
+        JSONObject jo = new JSONObject();
+        HashMapsTest myHashObject = new HashMapsTest();
+        System.out.println((myHashObject.addCities().toString()));
+        
+        Iterator<Entry<Integer,String>> iter = citiesMap.entrySet().iterator();
+        while(iter.hasNext()){
+           
+        }
+    }
+    
+}

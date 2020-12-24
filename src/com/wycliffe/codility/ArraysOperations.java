1d03f128840fc446b510c0f71177526a7256aac5
agwanyanjaba <bonkewycliffe@yahoo.com>
2020-12-24 21:26:33 +0300

    Modified ArrayOperations

--- src/com/wycliffe/codility/ArraysOperations.java
+++ src/com/wycliffe/codility/ArraysOperations.java
@@ -10,5 +10,36 @@ package com.wycliffe.codility;
  * @author WOCHIENG1
  */
 public class ArraysOperations {
-    
+
+    public int removeElement(int[] nums, int value) {
+        if (nums.length == 0) {
+            return 0;
+
+        }
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != value) {
+                nums[i] = nums[j];
+                i++;
+
+            }
+        }
+        System.out.println("#####"+i);
+        for(int elements: nums){
+            System.out.println("<<<<<<"+nums[elements]);
+        }
+        
+        return i;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = {3, 2, 2, 3};
+        ArraysOperations object = new ArraysOperations();
+        int numsLength = object.removeElement(nums, 3);
+        System.out.println("+++++" + numsLength);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(">>>>" + nums[i]);
+        }
+
+    }
 }

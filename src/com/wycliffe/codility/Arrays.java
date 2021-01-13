9685644471f00345d3c7a46ed0fcaf40c6bace72
agwanyanjaba <bonkewycliffe@yahoo.com>
2021-01-13 20:32:57 +0300

    Modified arrays.java

--- src/com/wycliffe/codility/Arrays.java
+++ src/com/wycliffe/codility/Arrays.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.wycliffe.codility;
 
 /**
@@ -12,27 +7,91 @@ package com.wycliffe.codility;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
+
 public class Arrays {
-    static int [] arrValues = {4,2,6,9,1,0,7};
+
+    static int[] arrValues = {1, 2, 3, 4, 5, 6, 7};
+    static int[] arrValuesTest = {1, 2, 3, 4, 5, 6, 7};
     static ArrayList<String> cars = new ArrayList<>();
-  
-    public static void main(String args[]){
+
+    public static int[] reverseArray(int[] Arr) {
+        int j = Arr.length;
+        int ArrTemp[] = new int[j];
+        for (int i = 0; i < Arr.length; i++) {
+            ArrTemp[i] = Arr[j - 1];
+
+            j--;
+        }
+        return ArrTemp;
+    }
+
+    //method to print an Array
+    public static void printArray(int[] arr) {
+        System.out.println("\n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    //function to rotate last element of the array
+    public static int[] rotateLastElement(int[] arr) {
+        int tempArray[] = new int[arr.length];
+        int last = arr[arr.length-1];
+        for (int i = arr.length - 2; i>=0; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[0] = last;
+        return arr;
+    }
+
+    //rotate array k number of times
+    public static int[] shiftArray(int[] arr1, int K) {
+        int shiftedArray[] = new int[arr1.length];
+        for (int i = 0; i < K; i++) {
+            shiftedArray = rotateLastElement(arr1);
+        }
+        return shiftedArray;
+    }
+
+    public static void main(String args[]) {
         cars.add("Volvo");
         cars.add("Toyota");
         cars.add("Bugharti");
-        
+
         Collections.sort(cars);
-        System.out.println("Sorted List: "+cars);
-        
-        //use for-each to printeach car item
-        for(String i:cars){
-            System.out.println("Values"+i);
+        System.out.println("Sorted List: " + cars);
+
+        //use for-each to print each car item
+        for (String i : cars) {
+            System.out.println("Values" + i);
         }
+
+//        Scanner sc = new Scanner(System.in);
+//        String s = sc.nextLine();
+//        
+//        System.out.println(s);
+        //Output the reversed array
+        int currentArray[] = reverseArray(arrValues);
+        int shiftedArray[] = rotateLastElement(arrValues);
+        int k_shiftedArray[] = shiftArray(arrValuesTest, 3);
         
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
+        System.out.println("START Reversed Array>>" );
+        for (int i = 0; i < arrValues.length; i++) {
+            System.out.println((currentArray[i]));
+
+        }
+         System.out.println("END Reversed Array>>" );
+        System.out.println("START Last element rotated left**" );
+        for (int i = 0; i < arrValues.length; i++) {
+            System.out.println((shiftedArray[i]));
+        }
+        System.out.println("END Last element rotated left**" );
         
-        System.out.println(s);
-        
+        System.out.println("START array rotated K times :::::::");
+        for (int i = 0; i < arrValuesTest.length; i++) {
+            System.out.println((k_shiftedArray[i]));
+        }
+        System.out.println("END array rotated K times ::::");
+
     }
 }
--- src/com/wycliffe/codility/BinaryGap.java
+++ src/com/wycliffe/codility/BinaryGap.java
@@ -36,11 +36,37 @@ public class BinaryGap {
 
         return finalCount;
     }
+    static public int solutionTwo(int N){
+        int finalCount = 0;
+        int tempCount = 0; 
+        
+        //convert the number into an array of binary string
+        char [] binaryNumber = Integer.toBinaryString(N).toCharArray();
+        
+        if(N==1){
+            return 0;
+        }
+        //loop through the array of chars, test for the values as '0' or '1'
+        //increase count if the value is 0
+        for(int i=0; i<binaryNumber.length; i++){
+            if(binaryNumber[i]=='0'){
+                tempCount++;
+            }
+            else if(binaryNumber[i]=='1'){
+                if(tempCount>finalCount){
+                    finalCount=tempCount;
+                    tempCount=0;
+                }
+            }
+        }
+        return finalCount;
+    }
 
     public static void main(String args[]) {
-        int N = 1376796946;
+        int N = 5681;
 
         System.out.println(">>>" + solution(N));
+        System.out.println(">>>" + solutionTwo(N));
     }
 
 }

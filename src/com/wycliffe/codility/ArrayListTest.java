721c0d9a897520bb09e9a6d5a2d8872bef671565
agwanyanjaba <bonkewycliffe@yahoo.com>
2021-01-16 17:54:15 +0300

    Added ArrayListTest

---
+++ src/com/wycliffe/codility/ArrayListTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wycliffe.codility;
+import java.util.ArrayList;
+/**
+ *
+ * @author WOCHIENG1
+ */
+public class ArrayListTest {
+    
+    
+    public static void main(String args []){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Red");
+        list.add(0,"Yellow");
+        
+        for(String i:list){
+            System.out.println("Elements are:"+i);
+        }
+        for(int i=0; i<list.size(); i++){
+            System.out.println("Elements are:"+list.get(i));
+        }
+    }
+}

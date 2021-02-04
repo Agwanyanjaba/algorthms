10c0e576299859333b15c5764f38a0fa17d02ef6
agwanyanjaba <bonkewycliffe@yahoo.com>
2021-02-04 08:17:53 +0300

    Added ignore SSL cert in CurlSendSMS

--- src/com/wycliffe/codility/simpleapps/CurlSendSMS.java
+++ src/com/wycliffe/codility/simpleapps/CurlSendSMS.java
@@ -8,8 +8,17 @@ package com.wycliffe.codility.simpleapps;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.cert.X509Certificate;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 /**
  *
@@ -34,9 +43,34 @@ public class CurlSendSMS {
 "   ]  \n" +
 " }' 'https://dtsvc.safaricom.com:8480/api/public/CMS/bulksms'";
     
+    
+    
     ProcessBuilder processBuilder = new ProcessBuilder(command); 
     
-    public static void main(String...vargs){
+    public static void main(String...vargs) throws Exception{
+        
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() { return null; }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+
+        } };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) { return true; }
+        };
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        /* End of the fix*/
+
+        //URL url = new URL("https://dtsvc.safaricom.com:8480/api/public/CMS/bulksms");
+        //URLConnection con = url.openConnection();
+        
         CurlSendSMS send = new CurlSendSMS();
         try {
             send.sendSMS();
